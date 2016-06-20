6a47e7991513cc3465e20fffe4c9c89047f67461
Magnus Hult <magnus@magnushult.se>
2016-06-20 17:04:57 +0200

    Add tests for branch

--- github_test.py
+++ github_test.py
@@ -3,17 +3,31 @@ import mock
 import github
 
 class TestGithub(object):
-    run_command_template = 'origin\tgit@github.com:%(username)s/%(repo)s.git (fetch)\norigin\tgit@github.com:%(username)s/%(repo)s.git (push)\n'
+    run_command_template = {
+        'remote': 'origin\tgit@github.com:%(username)s/%(repo)s.git (fetch)\norigin\tgit@github.com:%(username)s/%(repo)s.git (push)\n',
+        'status': 'On branch %(branch)s\nUntracked files: ...'
+    }
 
     def run_command_mock(self, command):
-        return self.run_command_template % {'username': self.username, 'repo': self.repo}
+        git_command = command.split(" ")[1]
+        return self.run_command_template[git_command] % {
+            'username': self.username,
+            'repo': self.repo,
+            'branch': self.branch
+        }
 
     def setup(self):
         self.username = 'hult'
         self.repo = 'github-open'
+        self.branch = 'branch-name'
         github.run_command = self.run_command_mock
 
     def test_repo(self):
         nt.assert_equals('hult/github-open', github.repo())
         self.repo = 'example.org'
         nt.assert_equals('hult/example.org', github.repo())
+
+    def test_branch(self):
+        nt.assert_equals('branch-name', github.branch())
+        self.branch = 'branch.name'
+        nt.assert_equals('branch.name', github.branch())
